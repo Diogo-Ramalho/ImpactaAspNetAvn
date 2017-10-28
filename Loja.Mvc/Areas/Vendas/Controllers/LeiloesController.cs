d8d2f959f83637ff6a60fb7820fd37c4f3affb0f
sa4802 <sa4802@2P48_02>
2017-10-28 17:50:34 -0200

    Aulas 5

--- Loja.Mvc/Areas/Vendas/Controllers/LeiloesController.cs
+++ Loja.Mvc/Areas/Vendas/Controllers/LeiloesController.cs
@@ -5,16 +5,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
 
 namespace Loja.Mvc.Areas.Vendas.Controllers
 {
+    [Authorize]
     public class LeiloesController : Controller
     {
         private LojaDbContext _db = new LojaDbContext();
 
         // GET: Vendas/Leiloes
+        [AllowAnonymous]
         public ActionResult Index()
         {
             return View(Mapeamento.Mapear(_db.Produtos.Where(i => i.EmLeilao).ToList()));
@@ -27,6 +30,14 @@ namespace Loja.Mvc.Areas.Vendas.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+
+            var usuario = (ClaimsIdentity) User.Identity;
+
+            if (!usuario.HasClaim(Modulo.Leilao.ToString(), Acao.Detalhar.ToString()))
+            {
+                return RedirectToAction("Login", "Account", new { Area = "" });
+            }
+
             Produto produto = _db.Produtos.Find(id);
             if (produto == null)
             {
--- Loja.Mvc/Startup.cs
+++ Loja.Mvc/Startup.cs
@@ -7,12 +7,13 @@ using Owin;
 
 namespace Loja.Mvc
 {
-    public class Startup
+    public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
             app.MapSignalR();
+            ConfigureAuth(app);
         }
     }
 }
